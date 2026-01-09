bcf5f4b0cafccd03569fab9810078f5d2a4c1fc7
NHDsun <huydatne@gmail.com>
2026-01-09 11:38:00 +0700

    đồ án thuật toán sắp xếp bảng 1.0

---
+++ Đồ Án Cơ Sở_65130410/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Đồ_Án_Cơ_Sở_65130410
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDraw = new System.Windows.Forms.Panel();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnQuickSort = new System.Windows.Forms.Button();
+            this.btnHeapSort = new System.Windows.Forms.Button();
+            this.btnMergeSort = new System.Windows.Forms.Button();
+            this.numSize = new System.Windows.Forms.NumericUpDown();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelDraw
+            // 
+            this.panelDraw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDraw.Location = new System.Drawing.Point(134, 81);
+            this.panelDraw.Name = "panelDraw";
+            this.panelDraw.Size = new System.Drawing.Size(428, 164);
+            this.panelDraw.TabIndex = 0;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(134, 303);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom.TabIndex = 0;
+            this.btnRandom.Text = "Start";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnQuickSort
+            // 
+            this.btnQuickSort.Location = new System.Drawing.Point(246, 303);
+            this.btnQuickSort.Name = "btnQuickSort";
+            this.btnQuickSort.Size = new System.Drawing.Size(75, 23);
+            this.btnQuickSort.TabIndex = 1;
+            this.btnQuickSort.Text = "QuickSort";
+            this.btnQuickSort.UseVisualStyleBackColor = true;
+            this.btnQuickSort.Click += new System.EventHandler(this.btnQuickSort_Click);
+            // 
+            // btnHeapSort
+            // 
+            this.btnHeapSort.Location = new System.Drawing.Point(375, 303);
+            this.btnHeapSort.Name = "btnHeapSort";
+            this.btnHeapSort.Size = new System.Drawing.Size(75, 23);
+            this.btnHeapSort.TabIndex = 2;
+            this.btnHeapSort.Text = "HeapSort";
+            this.btnHeapSort.UseVisualStyleBackColor = true;
+            this.btnHeapSort.Click += new System.EventHandler(this.btnHeapSort_Click);
+            // 
+            // btnMergeSort
+            // 
+            this.btnMergeSort.Location = new System.Drawing.Point(487, 303);
+            this.btnMergeSort.Name = "btnMergeSort";
+            this.btnMergeSort.Size = new System.Drawing.Size(75, 23);
+            this.btnMergeSort.TabIndex = 3;
+            this.btnMergeSort.Text = "MergeSort";
+            this.btnMergeSort.UseVisualStyleBackColor = true;
+            this.btnMergeSort.Click += new System.EventHandler(this.btnMergeSort_Click);
+            // 
+            // numSize
+            // 
+            this.numSize.Location = new System.Drawing.Point(601, 66);
+            this.numSize.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numSize.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numSize.Name = "numSize";
+            this.numSize.Size = new System.Drawing.Size(120, 22);
+            this.numSize.TabIndex = 4;
+            this.numSize.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(140, 33);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(84, 16);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Loại sắp xếp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.numSize);
+            this.Controls.Add(this.btnMergeSort);
+            this.Controls.Add(this.btnHeapSort);
+            this.Controls.Add(this.btnQuickSort);
+            this.Controls.Add(this.panelDraw);
+            this.Name = "Form1";
+            this.Text = "Đồ Án NHD_65130410";
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDraw;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnQuickSort;
+        private System.Windows.Forms.Button btnHeapSort;
+        private System.Windows.Forms.Button btnMergeSort;
+        private System.Windows.Forms.NumericUpDown numSize;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Đồ Án Cơ Sở_65130410/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Đồ_Án_Cơ_Sở_65130410
+{
+    public partial class Form1 : Form
+    {
+        int[] arr;
+        Random rd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(30, 30, 30);
+            this.Font = new Font("Segoe UI", 10);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Minh họa giải thuật sắp xếp";
+
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            int n = (int)numSize.Value;
+            arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = rd.Next(10, 300);
+
+            DrawArray();
+        }
+
+        void DrawArray(int a = -1, int b = -1)
+        {
+            if (arr == null || arr.Length == 0) return;
+
+            panelDraw.Refresh();
+            Graphics g = panelDraw.CreateGraphics();
+            int w = panelDraw.Width / arr.Length;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Brush br = Brushes.Blue;
+                if (i == a || i == b) br = Brushes.Red;
+
+                g.FillRectangle(
+                    br,
+                    i * w + 3,
+                    panelDraw.Height - arr[i],
+                    w - 6,
+                    arr[i]
+                );
+
+            }
+        }
+
+
+        async Task Delay()
+        {
+            await Task.Delay(200);
+        }
+
+        async Task QuickSort(int l, int r)
+        {
+            if (l >= r) return;
+
+            int pivot = arr[(l + r) / 2];
+            int i = l, j = r;
+
+            while (i <= j)
+            {
+                while (arr[i] < pivot) i++;
+                while (arr[j] > pivot) j--;
+
+                if (i <= j)
+                {
+                    Swap(i, j);
+                    DrawArray(i, j);
+                    await Delay();
+                    i++;
+                    j--;
+                }
+            }
+
+            await QuickSort(l, j);
+            await QuickSort(i, r);
+        }
+
+        async Task HeapSort()
+        {
+            int n = arr.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+                await Heapify(n, i);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                Swap(0, i);
+                DrawArray(0, i);
+                await Delay();
+                await Heapify(i, 0);
+            }
+        }
+
+        async Task Heapify(int n, int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if (l < n && arr[l] > arr[largest]) largest = l;
+            if (r < n && arr[r] > arr[largest]) largest = r;
+
+            if (largest != i)
+            {
+                Swap(i, largest);
+                DrawArray(i, largest);
+                await Delay();
+                await Heapify(n, largest);
+            }
+        }
+
+        async Task MergeSort(int l, int r)
+        {
+            if (l >= r) return;
+
+            int m = (l + r) / 2;
+            await MergeSort(l, m);
+            await MergeSort(m + 1, r);
+            await Merge(l, m, r);
+        }
+
+        async Task Merge(int l, int m, int r)
+        {
+            int[] temp = new int[r - l + 1];
+            int i = l, j = m + 1, k = 0;
+
+            while (i <= m && j <= r)
+                temp[k++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+
+            while (i <= m) temp[k++] = arr[i++];
+            while (j <= r) temp[k++] = arr[j++];
+
+            for (int x = 0; x < temp.Length; x++)
+            {
+                arr[l + x] = temp[x];
+                DrawArray(l + x, -1);
+                await Delay();
+            }
+        }
+
+        void Swap(int i, int j)
+        {
+            int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+        }
+
+        private async void btnQuickSort_Click(object sender, EventArgs e)
+        {
+            await QuickSort(0, arr.Length - 1);
+            lblStatus.Text = "Quick Sort xong";
+        }
+
+        private async void btnHeapSort_Click(object sender, EventArgs e)
+        {
+            await HeapSort();
+            lblStatus.Text = "Heap Sort xong";
+        }
+
+        private async void btnMergeSort_Click(object sender, EventArgs e)
+        {
+            await MergeSort(0, arr.Length - 1);
+            lblStatus.Text = "Merge Sort xong";
+        }
+        void StyleButton(Button b, Color c)
+        {
+            b.FlatStyle = FlatStyle.Flat;
+            b.FlatAppearance.BorderSize = 0;
+            b.BackColor = c;
+            b.ForeColor = Color.White;
+            b.Height = 38;
+            StyleButton(btnRandom, Color.FromArgb(52, 152, 219));
+            StyleButton(btnQuickSort, Color.FromArgb(46, 204, 113));
+            StyleButton(btnHeapSort, Color.FromArgb(241, 196, 15));
+            StyleButton(btnMergeSort, Color.FromArgb(155, 89, 182));
+
+        }
+
+    }
+}
